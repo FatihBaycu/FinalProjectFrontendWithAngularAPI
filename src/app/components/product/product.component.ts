724c64b4b533fd3104a28857a1e9ee0f2b8849d9
example <email@example.com>
2021-03-12 15:12:17 +0300

    .

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Product } from 'src/app/models/product';
+import { Product } from 'src/app/models/Product/product';
 
 @Component({
   selector: 'app-product',
