204f8955bc202e6b2d1f85baeef2c7c6da408304
example <email@example.com>
2021-03-12 15:10:35 +0300

    .

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Product } from 'src/app/models/product';
 
 @Component({
   selector: 'app-product',
@@ -7,17 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductComponent implements OnInit {
 
-  product:any={productId:1,productName:"Bardak",categoryId:1,unitPrice:5};
-  product2:any={productId:2,productName:"Laptop",categoryId:1,unitPrice:5};
-  product3:any={productId:3,productName:"Mouse",categoryId:1,unitPrice:5};
-  product4:any={productId:4,productName:"Camera",categoryId:1,unitPrice:5};
-  product5:any={productId:4,productName:"Notebook",categoryId:1,unitPrice:5};
+  // product:any={productId:1,productName:"Bardak",categoryId:1,unitPrice:5,unitInStock:10};
+  // product2:any={productId:2,productName:"Laptop",categoryId:1,unitPrice:5};
+  // product3:any={productId:3,productName:"Mouse",categoryId:1,unitPrice:5};
+  // product4:any={productId:4,productName:"Camera",categoryId:1,unitPrice:5};
+  // product5:any={productId:4,productName:"Notebook",categoryId:1,unitPrice:5};
+  //products=[this.product,this.product2,this.product3,this.product4,this.product5]
+  ////////////////////// lcoal çalışma mantığı bu şekldeydi./////////////////////////////////
 
-  products=[this.product,this.product2,this.product3,this.product4,this.product5]
+
+    products:Product[]=[];
 
   constructor() { }
 
   ngOnInit(): void {
   }
-
 }
---
+++ src/app/models/Product/product.ts
@@ -0,0 +1,7 @@
+export interface Product{
+    productId:number;
+    categoryId:number;
+    unitPrice:number;
+    unitInStock:number;
+    productName:string;
+}
